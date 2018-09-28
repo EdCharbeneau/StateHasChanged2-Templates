5321163a1b1a41cb64a8e7a599a58798fb04b377
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-09-28 15:24:55 -0400

    StateHasChanged Ep2 Blazor 0.6.0 Templates

---
+++ StateHasChanged2/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateHasChanged2.Model
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string AvatarUrl { get; set; }
+    }
+}
